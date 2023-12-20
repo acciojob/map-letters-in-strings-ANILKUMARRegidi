7c9cf491cc590ce02cce586538bad2e8cb41a611
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2023-12-20 12:17:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+function mapLetters(word) {
+  const letterIndexes = {};
+
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+
+    if (!letterIndexes[letter]) {
+      letterIndexes[letter] = [];
+    }
+
+    letterIndexes[letter].push(i);
+  }
+
+  return letterIndexes;
+}
+
+// Get user input using prompt
+const word = prompt("Enter word");
+
+// Call the mapLetters function with the user input
+const result = mapLetters(word);
+
+// Display the result using alert
+alert(JSON.stringify(result));
